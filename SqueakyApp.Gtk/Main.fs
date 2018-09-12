4ebeb73179bf5a4b9af593a04fdb9d33b29c0f09
Jim Bennett <jim.bennett@microsoft.com>
2018-09-12 14:38:14 +0100

    First cut

---
+++ SqueakyApp.Gtk/Main.fs
@@ -0,0 +1,20 @@
+﻿namespace SqueakyApp.Gtk
+    module Main =
+
+        open System
+        open Xamarin.Forms
+        open Xamarin.Forms.Platform.GTK
+
+        [<EntryPoint>]
+        let Main(args) =
+            Gtk.Application.Init()
+            Forms.Init()
+
+            let app = new SqueakyApp.App()
+            let window = new FormsWindow()
+            window.LoadApplication(app)
+            window.SetApplicationTitle("Hello Fabulous Linux")
+            window.Show();
+
+            Gtk.Application.Run()
+            0
