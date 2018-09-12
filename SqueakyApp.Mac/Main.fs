c08120172997ef2fa881f8933119b6cdb3e4a070
Jim Bennett <jimbobbennett@mac.com>
2018-09-12 16:46:06 +0100

    Added missing set of shared delegate

--- SqueakyApp.Mac/Main.fs
+++ SqueakyApp.Mac/Main.fs
@@ -6,5 +6,6 @@ module main =
     [<EntryPoint>]
     let main args =
         NSApplication.Init ()
+        NSApplication.SharedApplication.Delegate <- new AppDelegate();
         NSApplication.Main (args)
         0
